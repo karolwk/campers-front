64d937f09ecc72162facbff72ee006951a4c9328
karolwk <krl@o2.pl>
2022-10-31 23:58:07 +0100

    cleanup startup data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import NavBar from '../components/nav/NavBar/NavBar';
+import { navLinks } from '../shared/links';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar navLinks={navLinks} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
