ce7b968eaae185afbdbaaa2980ceed289f3b85aa
karolwk <krl@o2.pl>
2022-10-31 23:58:18 +0100

    added dummy links

---
+++ shared/links.ts
@@ -0,0 +1,23 @@
+export const navLinks = [
+  {
+    name: 'Główna',
+    url: '#',
+  },
+  {
+    name: 'O nas',
+    url: '#',
+  },
+  {
+    name: 'Kampery',
+    url: '#',
+  },
+  {
+    name: 'Warunki wynajmu',
+    url: '#',
+  },
+
+  {
+    name: 'Kontakt',
+    url: '#',
+  },
+];
