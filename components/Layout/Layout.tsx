8c91a673b3e0a0b62c62b5708c205db82856b53c
karolwk <krl@o2.pl>
2022-11-01 20:12:12 +0100

    added Layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,3 +1,28 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+
+import styles from '../../styles/Home.module.css';
+import React from 'react';
+
+type Props = {
+  children: React.ReactNode;
+  title: string;
+  description: string;
+};
+
+const Layout = ({ children, title, description }: Props) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
