9a6930997763bebbd22f49b5f8f22e20f8955502
karolwk <krl@o2.pl>
2022-11-02 20:32:31 +0100

    cleanup starter files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,22 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Layout from '../components/Layout/Layout';
 import NavBar from '../components/nav/NavBar/NavBar';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Kampery na wynajem</title>
-        <meta name="description" content="Kampery na wynajem" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}></main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+    <Layout title="Kampery na wynajem" description="Wynajem kamperow Wieliczka">
+      <Image src="/images/background.jpg" alt="dia" layout="fill" />
+      <h1>
+        {[...new Array(122)]
+          .map((ele) => {
+            return 'Nam provident pariatur pariatur amet vero. Et consequatur modi ut molestiae assumenda voluptas numquam doloremque. Et reprehenderit nostrum quisquam nisi ipsum tempore.';
+          })
+          .join('\n')}
+      </h1>
+    </Layout>
   );
 };
 
