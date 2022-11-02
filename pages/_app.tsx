24853d0bf5b8eac6da98273dbb741cddac63ac0d
karolwk <krl@o2.pl>
2022-11-02 20:33:02 +0100

    added Footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import NavBar from '../components/nav/NavBar/NavBar';
 import { navLinks } from '../shared/links';
+import Footer from '../components/Footer/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <NavBar navLinks={navLinks} />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
