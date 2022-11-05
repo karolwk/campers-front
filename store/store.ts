ae61a2b2b4bb209ce6f157c4dd717c248ae6b960
karolwk <krl@o2.pl>
2022-11-05 23:56:31 +0100

    added logic for store

--- store/store.ts
+++ store/store.ts
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './authSlice';
+import footerReducer from './footerSlice';
 import { createWrapper, HYDRATE } from 'next-redux-wrapper';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    footer: footerReducer,
   },
 });
 
