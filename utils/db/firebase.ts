9cb1f76f12727e4cf1fe2c18df0176424d3f9fd6
karolwk <krl@o2.pl>
2022-11-07 23:51:07 +0100

    added firebase config

--- utils/db/firebase.ts
+++ utils/db/firebase.ts
@@ -1,9 +1,23 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// Here comes your firebase configration
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+import admin from 'firebase-admin';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+if (!admin.apps.length) {
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert(firebaseConfig),
+    });
+  } catch (error: any) {
+    console.log('Firebase admin initialization error', error.stack);
+  }
 }
-const firestore = firebase.firestore();
-export { firestore };
+export default admin.firestore();
