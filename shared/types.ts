b79f543b85c9e79cd29f9cca7241630f1c205ea8
karolwk <krl@o2.pl>
2022-11-07 23:52:13 +0100

    added types for footer settings

--- shared/types.ts
+++ shared/types.ts
@@ -1,1 +1,14 @@
 export type Navlinks = { name: string; url: string }[];
+
+// Define a type for the slice state
+export interface FooterState {
+  logoURL: string;
+  email: string;
+  phone: string;
+  companyName: string;
+  companyaddress: string;
+  facebook: string | null;
+  instagram: string | null;
+  pinterest: string | null;
+  twitter: string | null;
+}
