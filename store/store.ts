294584c0693ee14dd43dd208a5afd6dd8d02d582
karolwk <krl@o2.pl>
2022-11-07 23:51:57 +0100

    cleaning

--- store/store.ts
+++ store/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import footerReducer from './footerSlice';
-import { createWrapper, HYDRATE } from 'next-redux-wrapper';
 
 export const store = configureStore({
   reducer: {
@@ -8,7 +7,6 @@ export const store = configureStore({
   },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+
 export type AppDispatch = typeof store.dispatch;
