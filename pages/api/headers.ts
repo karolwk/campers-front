71018396f4e87ce49db2e695fa7af9b5a0b61aff
karolwk <krl@o2.pl>
2022-11-09 23:54:53 +0100

    some changes to responses

--- pages/api/headers.ts
+++ pages/api/headers.ts
@@ -1,19 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import db from '../../utils/db/firebase';
-
-type Data = {
-  name: string;
-};
+import { FooterState } from '../../shared/types';
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<FooterState>
 ) {
   try {
-    const entries = await db.collection('settings').get();
-    const entriesData = entries.docs.map((entry) => entry.data());
+    const doc = await db
+      .collection('settings')
+      .doc('4qep1ITrkPDrxDRnaeYy')
+      .get();
 
-    res.status(200).json(entries);
+    if (!doc.exists) {
+      res.status(404).end();
+    } else {
+      res.status(200).json(doc.data() as FooterState);
+    }
   } catch (error) {
     res.status(400).end();
   }
