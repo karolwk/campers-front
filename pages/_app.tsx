ef13d5f7a467672d4239e4fc1c4a9d8cbe35dafb
karolwk <krl@o2.pl>
2022-11-11 23:52:20 +0100

    added provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,16 @@ import type { AppProps } from 'next/app';
 import NavBar from '../components/nav/NavBar/NavBar';
 import { navLinks } from '../shared/links';
 import Footer from '../components/Footer/Footer';
+import { Provider } from 'react-redux';
+import { store } from '../store/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider store={store}>
       <NavBar navLinks={navLinks} />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </Provider>
   );
 }
 
