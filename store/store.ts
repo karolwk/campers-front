b535c05231b18166a7c454ba861a9e34ccf74c98
karolwk <krl@o2.pl>
2022-11-11 23:51:01 +0100

    update redux store

--- store/store.ts
+++ store/store.ts
@@ -1,11 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
 import footerReducer from './footerSlice';
+
+import { localApi } from '../services/localApi';
 
 export const store = configureStore({
   reducer: {
+    [localApi.reducerPath]: localApi.reducer,
     footer: footerReducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 
