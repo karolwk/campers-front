6d224f25b5bb5190517366434bfa4ae9ab1b74cb
karolwk <krl@o2.pl>
2022-11-13 23:52:59 +0100

    added 'kontakt' page

---
+++ pages/kontakt.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import Layout from '../components/Layout/Layout';
+
+const Kontakt: NextPage = () => {
+  return (
+    <Layout
+      title="Kampery na wynajem - kontakt"
+      description="Wynajem kamperow Wieliczka - kontakt"
+    >
+      {/* <Image src="/images/background.jpg" alt="dia" layout="fill" /> */}
+
+      <h1>Kontakt</h1>
+    </Layout>
+  );
+};
+
+export default Kontakt;
