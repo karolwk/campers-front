f20c7fcb4bdcd03080b93303147019caf1d3826e
karolwk <krl@o2.pl>
2022-11-15 23:41:02 +0100

    added static props

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Layout from '../components/Layout/Layout';
+import { wrapper } from '../store/store';
+import { useFetchPageDataQuery } from '../services/pageSettingsApi';
 
 const Kontakt: NextPage = () => {
   return (
@@ -9,10 +11,24 @@ const Kontakt: NextPage = () => {
       description="Wynajem kamperow Wieliczka - kontakt"
     >
       {/* <Image src="/images/background.jpg" alt="dia" layout="fill" /> */}
+      {}
 
       <h1>Kontakt</h1>
     </Layout>
   );
 };
+
+export const getStaticProps = wrapper.getStaticProps((store) => async () => {
+  const { data } = useFetchPageDataQuery(null);
+
+  console.log('State on server', store.getState());
+  console.log(data);
+
+  return {
+    props: {
+      data,
+    },
+  };
+});
 
 export default Kontakt;
