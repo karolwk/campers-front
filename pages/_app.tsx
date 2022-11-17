e617ddcc4d8cd40c715265a5d202edf11b4ccca5
karolwk <krl@o2.pl>
2022-11-17 23:38:10 +0100

    added global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,13 @@ import { navLinks } from '../shared/links';
 import Footer from '../components/Footer/Footer';
 import { wrapper } from '../store/store';
 import { Provider } from 'react-redux';
+import { CssBaseline } from '@mui/material';
 
 function MyApp({ Component, ...rest }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(rest);
   return (
     <Provider store={store}>
+      <CssBaseline />
       <NavBar navLinks={navLinks} />
       <Component {...props.pageProps} />
       <Footer />
