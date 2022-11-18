db51bbaabbbbd0c3456428457d13fa4ce32ba494
karolwk <krl@o2.pl>
2022-11-18 23:44:35 +0100

    changed breakpoint visibility

--- components/dialogs/Drawer/MobileDrawer.tsx
+++ components/dialogs/Drawer/MobileDrawer.tsx
@@ -30,7 +30,7 @@ const MobileDrawer = ({
           keepMounted: true, // Better open performance on mobile.
         }}
         sx={{
-          display: { xs: 'block', sm: 'none' },
+          display: { xs: 'block', md: 'none' },
           '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
         }}
       >
