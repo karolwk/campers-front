8e44838655a734da3a259f58e77bd2e73eb95228
karolwk <krl@o2.pl>
2022-11-20 23:55:47 +0100

    added theming

---
+++ styles/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1C6758',
+    },
+  },
+});
+export default theme;
