0f41bf99919f4eae315581fc96f21f04cf21dc8e
karolwk <krl@o2.pl>
2022-11-22 23:40:12 +0100

    style changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,26 +8,26 @@ import db, { fetchFBData } from '../utils/db/firebase';
 import { doc, getDoc } from 'firebase/firestore';
 import { setEnt } from '../store/pageDataSlice';
 import { PageDataState } from '../shared/types';
+import { Container } from '@mui/material';
 
 const Home: NextPage = () => {
   return (
     <Layout title="Kampery na wynajem" description="Wynajem kamperow Wieliczka">
       {/* <Image src="/images/background.jpg" alt="dia" layout="fill" /> */}
-
-      <h1>
+      <Container component="section">
         {[...new Array(122)]
           .map((ele) => {
             return 'Nam provident pariatur pariatur amet vero. Et consequatur modi ut molestiae assumenda voluptas numquam doloremque. Et reprehenderit nostrum quisquam nisi ipsum tempore.';
           })
           .join('\n')}
-      </h1>
+      </Container>
     </Layout>
   );
 };
 
 export const getStaticProps = wrapper.getStaticProps((store) => async () => {
   const dupa = process.env.FIREBASE_DB_PAGEDATA_COL;
-  const docSnap = await fetchFBData(
+  const docSnap = await fetchFBData<PageDataState>(
     db,
     process.env.FIREBASE_DB_PAGEDATA_COL as string,
     process.env.FIREBASE_DB_PAGEDATA_DOC as string
