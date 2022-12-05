d1f7fd9c8902ce92a0d6bd94315e253bb9c108a8
karolwk <krl@o2.pl>
2022-12-05 23:53:24 +0100

    added external pathname for images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/kampery-kamil-cms.appspot.com/o/*',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
