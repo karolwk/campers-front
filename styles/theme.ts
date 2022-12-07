97a23478e7a01ff64cf54aab9953a18ade21ba20
karolwk <krl@o2.pl>
2022-12-07 23:42:38 +0100

    added fonts

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,6 +1,20 @@
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 const theme = createTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
   palette: {
     primary: {
       main: '#1C6758',
