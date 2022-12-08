ae8f036ce14a61cda3b0ce4995b08b4082c74a6b
karolwk <krl@o2.pl>
2022-12-08 23:41:36 +0100

    added new databes queries

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,24 @@
 import type { NextPage } from 'next';
-
 import Image from 'next/image';
-import Layout from '../components/Layout/Layout';
-
+import Layout from '../components/layouts/Layout/Layout';
 import { wrapper } from '../store/store';
 import db, { fetchFBData } from '../utils/db/firebase';
 import { doc, getDoc } from 'firebase/firestore';
 import { setEnt } from '../store/pageDataSlice';
 import { PageDataState } from '../shared/types';
 import { Box, Container, Typography } from '@mui/material';
-import { ref } from 'firebase/storage';
+import styles from '../styles/Home.module.css';
+import { MainPageData } from '../shared/types';
+import IconCard from '../components/cards/IconCard/IconCard';
+import ReactMarkdown from 'react-markdown';
 
 type HomeProps = {
-  mainPage: {};
+  mainPage: MainPageData;
+  test: MainPageData;
 };
 
-const Home: NextPage = () => {
+const Home: NextPage<HomeProps> = ({ test, mainPage }) => {
+  console.log(test);
   return (
     <Layout title="Kampery na wynajem" description="Wynajem kamperow Wieliczka">
       <Box sx={{ position: 'relative', minHeight: '40vw' }}>
@@ -42,37 +45,89 @@ const Home: NextPage = () => {
           Wynajmnij kampera
         </Typography>
       </Box>
+
       <Container component="section">
-        {[...new Array(22)]
-          .map((ele) => {
-            return 'Nam provident pariatur pariatur amet vero. Et consequatur modi ut molestiae assumenda voluptas numquam doloremque. Et reprehenderit nostrum quisquam nisi ipsum tempore.';
-          })
-          .join('\n')}
+        <Typography variant="h2">Dlaczego my</Typography>
+        <Box className={styles.icons}>
+          {mainPage.icons.map((icon) => {
+            return (
+              <IconCard
+                key={icon.title}
+                iconURL={icon.iconURL}
+                iconTitle={icon.title}
+                iconDescription={icon.description}
+              />
+            );
+          })}
+        </Box>
+      </Container>
+      <Box sx={{ backgroundColor: '#EEF2E6' }}>
+        <Container component="section">
+          <Typography variant="h2">{mainPage.teaserTitle}</Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+
+              gap: '100px',
+            }}
+          >
+            <Image
+              src="/images/kampery-na-wynajem-o-nas.jpg"
+              width="318"
+              height="378"
+              alt={mainPage.teaserTitle + ' obraz'}
+            ></Image>
+            <Box sx={{ maxWidth: '512px' }}>
+              <ReactMarkdown>{mainPage.teaserContent}</ReactMarkdown>
+            </Box>
+          </Box>
+        </Container>
+      </Box>
+      <Container>
+        <Typography variant="h2">{mainPage.campersTitle}</Typography>
+        <Typography variant="subtitle1">
+          {mainPage.campersDescription}
+        </Typography>
       </Container>
     </Layout>
   );
 };
 
 export const getStaticProps = wrapper.getStaticProps((store) => async () => {
+  // propagating redux store with page settings data
   const docSnap = await fetchFBData<PageDataState>(
     db,
     process.env.FIREBASE_DB_PAGEDATA_COL as string,
     process.env.FIREBASE_DB_PAGEDATA_DOC as string
   );
+  store.dispatch(setEnt(docSnap as PageDataState));
 
+  // geting main page data
   const docMainPage = doc(
     db,
-    process.env.FIREBASE_DB_PAGEDATA_COL as string,
-    process.env.FIREBASE_DB_PAGEDATA_DOC as string
+    process.env.FIREBASE_DB_MAINPAGE_COL as string,
+    process.env.FIREBASE_DB_MAINPAGE_DOC as string
   );
   const docMainPageSnap = await getDoc(docMainPage);
 
-  store.dispatch(setEnt(docSnap as PageDataState));
+  let test = docMainPageSnap.data() as any;
+
+  test.faq = await Promise.all(
+    //@ts-ignore
+    test.faq.map(async (ele) => {
+      const testSnap = await getDoc(ele);
+      return testSnap.data();
+    })
+  );
+
+  let mainPage = JSON.stringify(docMainPageSnap.data());
+  mainPage = JSON.parse(mainPage);
 
   return {
     props: {
-      appProp: docSnap,
-      mainPage: docMainPageSnap.data(),
+      mainPage: mainPage,
+      test: test,
     },
   };
 });
