28092a55563a1c3eebb27273af43e49cffe51857
karolwk <krl@o2.pl>
2022-12-18 23:53:08 +0100

    added helper methods for url slugs

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 export const formatPhone = (number: string): string => {
   let newNumber = number.replace(/\D+/g, '');
   if (newNumber.length !== 9) return number;
@@ -11,12 +13,36 @@ export const formatPhone = (number: string): string => {
   return newNumber;
 };
 
+// helper function to convert image src path to Google Cloud Storage address
 export const formatPathtoGCS = (path: string): string => {
-  // helper function to convert image src path to Google Cloud Storage address
-
   const bucket =
     'https://firebasestorage.googleapis.com/v0/b/kampery-kamil-cms.appspot.com/o/';
   const encodedPath = path.replace('/', '%2F');
 
   return bucket + encodedPath + '?alt=media';
+};
+
+export const makeURLfromName = (name: string): string => {
+  return name.trim().toLowerCase().replace(/ /g, '-');
+};
+
+// make params array for getStaticPaths, from array of strings
+
+export const makeURLPaths = (
+  names: string[]
+): {
+  params: {
+    id: string;
+  };
+}[] => {
+  // Making sure that there is no
+  const uniqeNames = _.uniq(names);
+
+  return uniqeNames.map((name) => {
+    return {
+      params: {
+        id: makeURLfromName(name),
+      },
+    };
+  });
 };
