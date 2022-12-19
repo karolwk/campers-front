cba9ca4b30374f8f5fd94b22500d9f0061bba21f
karolwk <krl@o2.pl>
2022-12-19 22:28:14 +0100

    added campers collection page

---
+++ pages/kampery.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import Layout from '../components/layouts/Layout/Layout';
+import { wrapper } from '../store/store';
+import db from '../utils/db/firebase';
+import { doc, getDoc } from 'firebase/firestore';
+import { setEnt } from '../store/pageDataSlice';
+import { PageDataState } from '../shared/types';
+import { Container } from '@mui/material';
+
+interface OtherProps {
+  appProp: string;
+}
+
+const Kampery: NextPage<OtherProps> = ({ appProp }) => {
+  return (
+    <Layout
+      title="Kampery na wynajem - kontakt"
+      description="Wynajem kamperow Wieliczka - kontakt"
+    >
+      <Container component="section">
+        {/* <Image src="/images/background.jpg" alt="dia" layout="fill" /> */}
+        {JSON.stringify(appProp)}
+
+        <h1>Kampery</h1>
+      </Container>
+    </Layout>
+  );
+};
+
+export const getStaticProps = wrapper.getStaticProps((store) => async () => {
+  const docRef = doc(
+    db,
+    process.env.FIREBASE_DB_PAGEDATA_COL as string,
+    process.env.FIREBASE_DB_PAGEDATA_DOC as string
+  );
+  const docSnap = await getDoc(docRef);
+  store.dispatch(setEnt(docSnap.data() as PageDataState));
+
+  return {
+    props: {
+      appProp: docSnap.data(),
+    },
+  };
+});
+
+export default Kampery;
