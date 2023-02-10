0347293c808bd811c51981877d02c485d9df3060
karolwk <krl@o2.pl>
2023-02-10 23:54:30 +0100

    refactor of Icon Component

--- components/cards/IconCard/IconCard.tsx
+++ components/cards/IconCard/IconCard.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Divider, Paper, Typography } from '@mui/material';
 import Image from 'next/image';
 import { formatPathtoGCS } from '../../../utils/helpers';
+import styles from './IconCard.module.css';
 
 type Props = {
   iconURL: string;
@@ -11,16 +12,20 @@ type Props = {
 
 const IconCard = ({ iconURL, iconTitle, iconDescription }: Props) => {
   return (
-    <Box>
+    <Paper elevation={5} className={styles.iconContainer}>
       <Image
         src={formatPathtoGCS(`${iconURL}`)}
         width={100}
         height={100}
         alt={iconTitle + ' icon'}
       ></Image>
-      <Typography>{iconTitle}</Typography>
+
+      <Typography variant="h5" className={styles.title}>
+        {iconTitle}
+      </Typography>
+      <Divider className={styles.divider} />
       <Typography>{iconDescription}</Typography>
-    </Box>
+    </Paper>
   );
 };
 
