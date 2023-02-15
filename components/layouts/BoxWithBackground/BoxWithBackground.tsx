02f952114bf8e7c4c52ea41cd5f955f55e399a9b
karolwk <krl@o2.pl>
2023-02-15 15:27:10 +0100

    added boxwithbg comp

---
+++ components/layouts/BoxWithBackground/BoxWithBackground.tsx
@@ -0,0 +1,31 @@
+import { Box, BoxProps, Container } from '@mui/material';
+import Image from 'next/image';
+import React, { FC, PropsWithChildren } from 'react';
+interface Props extends BoxProps {
+  bgImage: string;
+}
+
+const BoxWithBackground: FC<PropsWithChildren<Props>> = ({
+  bgImage = '',
+  children,
+  ...rest
+}) => {
+  return (
+    <Box
+      sx={{ position: 'relative', zIndex: 0, paddingBottom: '4rem' }}
+      {...rest}
+    >
+      <Image
+        src={bgImage}
+        alt="Background image"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="bottom"
+        priority
+      />
+      {children}
+    </Box>
+  );
+};
+
+export default BoxWithBackground;
