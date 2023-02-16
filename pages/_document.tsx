b91a0fb17a2426e7c6c51f76b963be2b594939f8
karolwk <krl@o2.pl>
2023-02-16 23:52:51 +0100

    added lang to document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="pl">
       <Head>
         <link
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
