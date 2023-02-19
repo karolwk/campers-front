25d44a4407a0b8ddeebba005a2a6dbe66d55cc27
karolwk <krl@o2.pl>
2023-02-19 23:55:19 +0100

    added map to kontakt page

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -5,23 +5,58 @@ import { wrapper } from '../store/store';
 import { fetchPageData } from '../utils/db/firebase';
 import { setEnt } from '../store/pageDataSlice';
 import { PageDataState } from '../shared/types';
-import { Container } from '@mui/material';
+import { Container, Typography, Paper, Box, Button } from '@mui/material';
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
+import styles from '../styles/Kontakt.module.css';
+import Map from '../components/ui/Map/Map';
+import MuiLink from '@mui/material/Link';
 
 interface OtherProps {
-  appProp: string;
+  appProp: PageDataState;
 }
 
 const Kontakt: NextPage<OtherProps> = ({ appProp }) => {
+  const InfoWindowText = () => {
+    return (
+      <>
+        <Typography fontWeight={500} marginBottom="1rem" align="center">
+          {appProp.companyName}
+        </Typography>
+
+        <Button href="https://goo.gl/maps/kmDoZT4ziohQ6hdY6" variant="outlined">
+          Ustaw trasę dojazdu
+        </Button>
+      </>
+    );
+  };
   return (
     <Layout
-      title="Kampery na wynajem - kontakt"
-      description="Wynajem kamperow Wieliczka - kontakt"
+      metaTitle="Kampery na wynajem - kontakt"
+      metaDescription="Wynajem kamperow Wieliczka - kontakt"
+      pageTitle="Kontakt"
+      pageSubtitle="Skontaktuj się z nami - z przyjemnością pomożemy!"
     >
-      <Container component="section">
-        {/* <Image src="/images/background.jpg" alt="dia" layout="fill" /> */}
-        {JSON.stringify(appProp)}
-
-        <h1>Kontakt</h1>
+      <Container component="section" sx={{ marginY: '2rem' }}>
+        <Grid2 container>
+          <Grid2 xs={12} md={6}>
+            <Box>
+              <Typography fontSize="1.2rem" fontWeight={500}>
+                {appProp.companyName}
+              </Typography>
+              <Typography>{appProp.companyaddress}</Typography>
+              <Typography>
+                {appProp.companyZipCode + ' ' + appProp.companyCity}
+              </Typography>
+            </Box>
+            <Paper elevation={2} sx={{ border: 'none' }}>
+              <Map
+                api="AIzaSyC0sfDBAdm6BZMKDuXu67APX6o18N_jGjg"
+                center={{ lat: 49.982276, lng: 20.076053 }}
+                infoWindowContent={<InfoWindowText />}
+              />
+            </Paper>
+          </Grid2>
+        </Grid2>
       </Container>
     </Layout>
   );
