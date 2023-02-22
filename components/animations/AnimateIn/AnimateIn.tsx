75a319c80457a3a01add9db814345e85f732ef0c
karolwk <krl@o2.pl>
2023-02-22 23:52:57 +0100

    minor fixes and additon of transition delay

--- components/animations/AnimateIn/AnimateIn.tsx
+++ components/animations/AnimateIn/AnimateIn.tsx
@@ -3,29 +3,33 @@ import React, { FC, PropsWithChildren, useEffect, useState } from 'react';
 import { BoxProps } from '@mui/material';
 import useOnScreen from '../../../hooks/useOnScreen';
 
-interface AnimateInProps extends BoxProps {
-  from: React.CSSProperties;
-  to: React.CSSProperties;
+export interface AnimateInProps extends BoxProps {
+  from?: React.CSSProperties;
+  to?: React.CSSProperties;
   transition?: string;
+  transitionDelay?: string;
   component: React.ElementType;
 }
 
 const AnimateIn: FC<PropsWithChildren<AnimateInProps>> = ({
-  from,
-  to,
+  from = {},
+  to = {},
   component: Component,
   children,
+  className,
+
   transition = '650ms ease-in-out',
+  transitionDelay = '0ms',
   ...rest
 }) => {
   const ref = React.useRef<HTMLDivElement>(null);
   const [rendered, setRendered] = useState(false);
   const onScreen = useOnScreen(ref);
+  const NoAnimationClass = 'NoAnimations';
 
   let dynamicStyles: React.CSSProperties = {};
-  if (!rendered) {
-    dynamicStyles = onScreen ? to : from;
-  }
+
+  dynamicStyles = rendered ? to : from;
 
   useEffect(() => {
     if (onScreen) {
@@ -36,7 +40,14 @@ const AnimateIn: FC<PropsWithChildren<AnimateInProps>> = ({
   return (
     <Component
       ref={ref}
-      style={{ transition: `${transition}`, ...dynamicStyles }}
+      className={
+        className ? `${className} + ${NoAnimationClass}` : NoAnimationClass
+      }
+      style={{
+        transition: `${transition}`,
+        transitionDelay: `${transitionDelay}`,
+        ...dynamicStyles,
+      }}
       {...rest}
     >
       {children}
