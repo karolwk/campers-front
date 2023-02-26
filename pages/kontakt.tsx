7c040a91f19147f3227dae0ccf4dc250cfb7bcf2
karolwk <krl@o2.pl>
2023-02-26 23:46:27 +0100

    wrapped contact form with recaptcha provider

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -10,6 +10,9 @@ import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 import styles from '../styles/Kontakt.module.css';
 import Map from '../components/ui/Map/Map';
 import MuiLink from '@mui/material/Link';
+import ContactForm from '../components/forms/ContactForm/ContactForm';
+
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 interface OtherProps {
   appProp: PageDataState;
@@ -29,6 +32,7 @@ const Kontakt: NextPage<OtherProps> = ({ appProp }) => {
       </>
     );
   };
+
   return (
     <Layout
       metaTitle="Kampery na wynajem - kontakt"
@@ -37,7 +41,7 @@ const Kontakt: NextPage<OtherProps> = ({ appProp }) => {
       pageSubtitle="Skontaktuj się z nami - z przyjemnością pomożemy!"
     >
       <Container component="section" sx={{ marginY: '2rem' }}>
-        <Grid2 container>
+        <Grid2 container spacing={2}>
           <Grid2 xs={12} md={6}>
             <Box>
               <Typography fontSize="1.2rem" fontWeight={500}>
@@ -50,11 +54,20 @@ const Kontakt: NextPage<OtherProps> = ({ appProp }) => {
             </Box>
             <Paper elevation={2} sx={{ border: 'none' }}>
               <Map
-                api="s"
+                api={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API as string}
                 center={{ lat: 49.982276, lng: 20.076053 }}
                 infoWindowContent={<InfoWindowText />}
               />
             </Paper>
+          </Grid2>
+          <Grid2 xs={12} md={6}>
+            <GoogleReCaptchaProvider
+              reCaptchaKey={
+                process.env.NEXT_PUBLIC_CLIENT_RECAPTACHA_API as string
+              }
+            >
+              <ContactForm />
+            </GoogleReCaptchaProvider>
           </Grid2>
         </Grid2>
       </Container>
