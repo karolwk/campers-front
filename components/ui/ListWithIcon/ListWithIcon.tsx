77a30713d4b8b584f0162bd1e2ed36d7326df8a1
karolwk <krl@o2.pl>
2023-03-20 20:42:08 +0100

    added new render func for shorter list

--- components/ui/ListWithIcon/ListWithIcon.tsx
+++ components/ui/ListWithIcon/ListWithIcon.tsx
@@ -12,23 +12,30 @@ import { MainAmenities } from '../../../shared/types';
 type Props = {
   items?: MainAmenities[];
   dense?: boolean;
+  shorter?: boolean;
 };
 
-const ListWithIcon = ({ items, dense = false }: Props) => {
+const ListWithIcon = ({ items, dense = false, shorter = false }: Props) => {
   if (!items) {
     return <></>;
   }
 
   return (
-    <List dense={dense}>
-      {items.map((item) => (
-        <ListItem key={item.name}>
-          <ListItemIcon>
-            <Icon>{item.icon}</Icon>
-          </ListItemIcon>
-          <ListItemText primary={item.name} />
-        </ListItem>
-      ))}
+    <List dense={dense ? dense : shorter}>
+      {items.map((item, index) => {
+        if (shorter && index > 3) {
+          return;
+        }
+
+        return (
+          <ListItem key={item.name}>
+            <ListItemIcon>
+              <Icon>{item.icon}</Icon>
+            </ListItemIcon>
+            <ListItemText primary={item.name} />
+          </ListItem>
+        );
+      })}
     </List>
   );
 };
